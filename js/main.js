bf4b10191b5948a8e8cf1e4ec07b95747b28e379
Komang Aswameda <aswameda18@gmail.com>
2026-07-01 12:43:06 +0800

    selesai header team

--- js/main.js
+++ js/main.js
@@ -17,3 +17,53 @@ clearBtn.addEventListener("click", () => {
   clearBtn.style.display = "none";
   input.focus();
 });
+
+// angka random dari 0
+const counters = document.querySelectorAll(".counter");
+
+function animateCounter(counter) {
+  const target = Number(counter.dataset.target);
+  let current = 0;
+
+  const increment = target / 250;
+
+  function update() {
+    current += increment;
+
+    if (current < target) {
+      counter.textContent = Math.floor(current);
+      requestAnimationFrame(update);
+    } else {
+      // Angka akhir
+      if (target >= 1000) {
+        counter.textContent = `${target / 1000}K+`;
+      } else if (target === 98) {
+        counter.textContent = `${target}%`;
+      } else {
+        counter.textContent = `${target}+`;
+      }
+    }
+  }
+
+  update();
+}
+
+const observer = new IntersectionObserver(
+  (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        animateCounter(entry.target);
+
+        // Supaya animasi cuma sekali
+        observer.unobserve(entry.target);
+      }
+    });
+  },
+  {
+    threshold: 0.9,
+  },
+);
+
+counters.forEach((counter) => {
+  observer.observe(counter);
+});
